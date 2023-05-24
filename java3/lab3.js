d606710e91338474d9594b32ff493e9ef854aa03
Sashenyka <126782943+SanyOkkkkk@users.noreply.github.com>
2023-05-24 21:41:21 +0300

    add

--- java3/lab3.js
+++ java3/lab3.js
@@ -22,22 +22,16 @@ export function  truncate(str, maxlength){
     str.slice(0, maxlength - 1) + '…' : str;
 }
 export function  camelize(str){
-    return str
-    .split('-') // разбивает 'my-long-word' на массив ['my', 'long', 'word']
-    .map(
-      // Переводит в верхний регистр первые буквы всех элементом массива за исключением первого
-      // превращает ['my', 'long', 'word'] в ['my', 'Long', 'Word']
-      (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
-    )
-    .join(''); // соединяет ['my', 'Long', 'Word'] в 'myLongWord'
+    return str.replace(/-/g, "");
 }
 export function  fibs(n){
     return (str.length > maxlength) ?
     str.slice(0, maxlength - 1) + '…' : str;
 }
 export function  arrReverseSorted(arr){
-    return (str.length > maxlength) ?
-    str.slice(0, maxlength - 1) + '…' : str;
+    return arr.sort(function (a, b) {
+        return b - a;
+    })
 }
 export function  unique(arr){
     return Array.from(new Set(arr));
